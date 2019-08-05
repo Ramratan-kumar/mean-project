5ea7282629c5a4fd625ff8ec8ef6ad8709b1eef9
ramratan <ramratankumar11@gmail.com>
2019-08-05 20:08:24 +0530

    jwt implemented

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -3,23 +3,63 @@ var userModel = require('../models/userModel');
 var passwordHash = require('password-hash');
 var fs = require('fs');
 var path = "D:/project/FileIngester/";
+const jwt = require('jsonwebtoken');
+let config = require('../config');
 
 module.exports = {
     registerUser: registerUser,
-    getUserDetails: getUserDetails
+    getUserDetails: getUserDetails,
+    login: login
 }
 
 async function registerUser(req, res) {
     try {
         req.body.password = passwordHash.generate(req.body.password);
-        user = new userModel(req.body);
-        await user.save();
-        res.send(user);
+        existingUser = await userModel.findOne({
+            $or: [{ email: req.body.email },
+            { username: req.body.username }]
+        }, { email: 1, username: 1 });
+        if (existingUser.email === req.body.email) {
+            return res.status(206).json({ message: "Email all ready exists." })
+        } else if (existingUser.username === req.body.username) {
+            return res.status(206).json({ message: "User Name all ready exists." })
+        } else {
+            user = new userModel(req.body);
+            await user.save();
+            res.status(200).json({ message: "User created successfully" });
+        }
 
     } catch (err) {
-        console.log(err);
+        console.error(err);
         res.status(400).send(err);
     }
+}
+
+async function login(req, res) {
+    try {
+        userEmail = req.body.email ? req.body.email.toLowerCase() : req.body.username.toLowerCase();
+        user = await userModel.findOne({
+
+            $or: [{ email: userEmail },
+            { username: userEmail }]
+        }, { password: 1 }
+
+        );
+        if (user && passwordHash.verify(req.body.password, user.password)) {
+            let token = jwt.sign({ username: userEmail },
+                config.secret,
+                {
+                    expiresIn: '1h' // expires in 24 hours
+                }
+            );
+            res.status(200).send({ token: token });
+        } else {
+            res.status(401).json({ message: "Password or username incorrect." })
+        }
+    } catch (err) {
+        res.status(400).send("Some thing went wrong.");
+    }
+
 }
 
 async function getUserDetails(req, res) {
--- server/router/users.js
+++ server/router/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var middleWare = require('../services/middleware');
 var userController = require('../controller/userController');
 /* GET users listing. */
-router.get('/', userController.getUserDetails);
+router.get('/',middleWare.verifyToken, userController.getUserDetails);
 router.post('/create', userController.registerUser);
+router.post('/login', userController.login);
 
 module.exports = router;
