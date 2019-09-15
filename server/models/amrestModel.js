dac687a837f14821d247d5b49266deaf517529ce
ramratan <ramratankumar11@gmail.com>
2019-09-15 14:31:37 +0530

    model deleted

--- server/models/amrestModel.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var amrestSchema = new Schema({}, { strict: false });
-var amrestFilesModel = mongoose.model('amrestFile', amrestSchema);
-module.exports ={amrestFilesModel:amrestFilesModel,amrestSchema:amrestSchema};
